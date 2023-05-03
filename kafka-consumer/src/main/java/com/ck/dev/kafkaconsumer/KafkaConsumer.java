dfd67c3e5c30af91d1845ef056fce9c4577959c9
kumarchandan98 <chandan.kumar1@smarsh.com>
2023-05-03 12:24:06 +0530

    kafka-binder consuming batches of messages

--- kafka-consumer/src/main/java/com/ck/dev/kafkaconsumer/KafkaConsumer.java
+++ kafka-consumer/src/main/java/com/ck/dev/kafkaconsumer/KafkaConsumer.java
@@ -4,16 +4,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 @Configuration
 public class KafkaConsumer {
 
     @Bean
-    public Consumer<Message<String>> messageConsumer() {
-        return message -> {
-            System.out.println(message.getHeaders());
-            System.out.println(message.getPayload());
+    public Consumer<Message<List<String>>> singleTopic_SinglePartition() {
+        return messages -> {
+            System.out.println("Consuming "+messages.getPayload().size()+" messages");
+//            System.out.println(message.getHeaders());
+            messages.getPayload().forEach(System.out::println);
+        };
+    }
+
+    @Bean
+    public Consumer<Message<List<String>>> singleTopic_MultiplePartition() {
+        return messages -> {
+            System.out.println("Consuming "+messages.getPayload().size()+" messages");
+//            System.out.println(message.getHeaders());
+            messages.getPayload().forEach(System.out::println);
+        };
+    }
+
+    @Bean
+    public Consumer<Message<List<String>>> multipleTopic_MultiplePartition() {
+        return messages -> {
+            System.out.println("Consuming "+messages.getPayload().size()+" messages");
+//            System.out.println(message.getHeaders());
+            messages.getPayload().forEach(System.out::println);
         };
     }
 }
